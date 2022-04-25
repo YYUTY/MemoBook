29686603ada2c6091794fb8c99246934d6a4145c
YYUTY <asa2siriaru@gmail.com>
2022-04-25 19:57:42 +0900

    add

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def init():
         va.setting=json.load(f)
     with open('./Setting/Language/'+str(va.setting["Language"])+'.json','r', encoding="utf-8") as f:
         va.lang=json.load(f)
+    with open('./Setting/Texture/Skin/'+str(va.setting["Skin"])+'.json','r',encoding="utf-8") as f:
+        va.skin=json.load(f)
 
 def main():
     va.root=tk.Tk()
     va.path=os.getcwd()
     va.root.title('MemoBook')
+    va.root.state('zoomed')
     va.root.geometry('1920x1080')
     va.nt=ft.File_tab(va.root)
+    va.nt.start()
     m=menu.Menu(tk.Menu(va.root))
     va.root.mainloop()
 
